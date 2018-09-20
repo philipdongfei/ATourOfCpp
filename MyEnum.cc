f7b33602f61a673112cd36e5482cfe90ec7eaacc
philiptung <philip.dongfei@gmail.com>
2018-09-20 15:00:03 +0800

    define operators for enumeration

--- MyEnum.cc
+++ MyEnum.cc
@@ -4,6 +4,17 @@
 enum class Color {red, blue, green};
 enum class Traffic_light{green, yellow, red};
 
+Traffic_light& operator++(Traffic_light& t)
+    // prefix increment: ++
+{
+    switch(t) {
+        case Traffic_light::green: return t=Traffic_light::yellow;
+        case Traffic_light::yellow: return t=Traffic_light::red;
+        case Traffic_light::red:    return t=Traffic_light::green;
+    }
+
+}
+
 int main(int argc, char *argv[])
 {
     Color col = Color::red;
